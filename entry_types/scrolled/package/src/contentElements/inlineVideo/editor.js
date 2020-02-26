6f572250ba4b92dc4d831d37884b951dae2e1e40
Daniel Kuhn <dannikuhn@gmail.com>
2020-02-26 12:00:00 +0100

    Make backdrop and inline videos configurable in editor

--- entry_types/scrolled/package/src/contentElements/inlineVideo/editor.js
+++ entry_types/scrolled/package/src/contentElements/inlineVideo/editor.js
@@ -1,8 +1,21 @@
 import {editor} from 'pageflow-scrolled/editor';
+import {CheckBoxInputView, SelectInputView} from 'pageflow/ui';
+import {FileInputView} from 'pageflow/editor';
 
 editor.contentElementTypes.register('inlineVideo', {
   configurationEditor() {
     this.tab('general', function() {
+      this.input('id', FileInputView, {
+        collection: 'video_files',
+        fileSelectionHandler: 'contentElementConfiguration',
+        positioning: false
+      });
+      this.input('autoplay', CheckBoxInputView);
+      this.input('controls', CheckBoxInputView);
+      this.input('position', SelectInputView, {
+        attributeTranslationKeyPrefixes: ['pageflow_scrolled.editor.inputs'],
+        values: ['inline', 'sticky', 'full']
+      });
     });
   }
 });
